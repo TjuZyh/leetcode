8d2364bebcb7559a1c2aada2fa7e0a4354abf2d6
TjuZyh <tju_zhaoyihan@163.com>
2022-08-21 11:42:48 +0800

    0821打卡：检查前缀

---
+++ leetcode/src/com/leetcode/DailyCheckIn/_0821/IsPrefixOfWord_1455.java
@@ -0,0 +1,34 @@
+package com.leetcode.DailyCheckIn._0821;
+
+import java.util.Arrays;
+
+/**
+ * @Author zyh
+ * @Date 2022/8/21 11:25
+ * @Version 1.0
+ */
+public class IsPrefixOfWord_1455 {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] sentenceArr = sentence.split(" ");
+        for(int i = 0 ; i < sentenceArr.length ; i++){
+            if(isPrefix(searchWord , sentenceArr[i])){
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static boolean isPrefix(String search , String origin){
+        if(search.length() > origin.length()){
+            return false;
+        }
+        if(origin.substring(0 , search.length()).equals(search)){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new IsPrefixOfWord_1455().isPrefixOfWord("hellohello hellohellohello" , "ell"));
+    }
+}
